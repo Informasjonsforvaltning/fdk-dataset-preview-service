7dc6745dc7e2bdb0c6dacbc5888edfad1f2e2cf2
Jeff Reiffers <jeffreiffers@gmail.com>
2022-11-24 13:04:34 +0100

    chore: use custom csrf repository

--- src/main/kotlin/no/fdk/dataset/preview/security/SecurityConfiguration.kt
+++ src/main/kotlin/no/fdk/dataset/preview/security/SecurityConfiguration.kt
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
@@ -32,12 +31,9 @@ open class SecurityConfiguration(
             authentication
         }
 
-        val csrfRepository = CookieCsrfTokenRepository.withHttpOnlyFalse()
-        csrfRepository.setCookieName("DATASET-PREVIEW-CSRF-TOKEN")
-
         http.cors().and()
             .csrf()
-            .csrfTokenRepository(csrfRepository)
+            .csrfTokenRepository(CustomCsrfTokenRepository.withHttpOnlyFalse())
             .and()
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
